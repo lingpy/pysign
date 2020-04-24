ed8fcfbaf36d6748b358d986d5bd3b5b9c8f197a
Justin Power <33174072+justopower@users.noreply.github.com>
2020-04-24 08:20:55 -0500

    Add files via upload

---
+++ tests/test_sign.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+# Example (1) most simple full sign transcription: 
+# one dominant hand, one orientation, one location, one movement
+
+{
+"   ": # segmented into types
+    {
+"symmetry": "",
+"dominant_hand": "", # hamfinger2
+"nondominant_hand": "",
+"orientation": "", # hamextfingeru,hampalmu
+"location": "", # hamchest
+"movement": "" # hammoveo
+    }
+}
