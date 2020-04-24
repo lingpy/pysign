53561e197e8005688242602c55a4d6091e202003
Justin Power <33174072+justopower@users.noreply.github.com>
2020-04-24 08:32:37 -0500

    Update test_sign.py

--- tests/test_sign.py
+++ tests/test_sign.py
@@ -5,16 +5,33 @@
 """
 
 # Example (1) most simple full sign transcription: 
-# one dominant hand, one orientation (must have two symbols), one location, one movement
+# one dominant hand, one orientation (must have two symbols), one location,
+# one movement
 
 {
 "   ": # segmented into types
     {
-"symmetry": "",
-"dominant_hand": "", # hamfinger2
-"nondominant_hand": "",
-"orientation": "", # hamextfingeru,hampalmu
-"location": "", # hamchest
-"movement": "" # hammoveo
+    "symmetry": "",
+    "dominant_hand": "", # hamfinger2
+    "nondominant_hand": "",
+    "orientation": "", # hamextfingeru,hampalmu
+    "location": "", # hamchest
+    "movement": "" # hammoveo
     }
+}
+
+# Example (2) most simple two-handed sign with symmetry: 
+# one symmetry symbol, one dominant hand, one orientation, one location, 
+# one movement
+
+{
+"    ":
+    {
+     "symmetry": "", #hamsymmpar
+     "dominant_hand": "", # hamfinger2
+     "nondominant_hand": "",
+     "orientation": "", # hamextfingeru,hampalmu
+     "location": "", # hamchest
+     "movement": "" # hammoveo
+     }
 }
