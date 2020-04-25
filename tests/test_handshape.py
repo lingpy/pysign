3d9d6754743e6d48fe85e331969fedfde6bb1951
Justin Power <33174072+justopower@users.noreply.github.com>
2020-04-25 13:05:45 -0500

    Add files via upload
    test regex to match handshapes

---
+++ tests/test_handshape.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+
+regex = r"[]([]|[]|[]|[])*"
+
+test_str = ""
+
+matches = re.finditer(regex, test_str)
+
+for matchNum, match in enumerate(matches, start=1):
+    
+    print ("Match {matchNum} was found at {start}-{end}: {match}".format(
+        matchNum = matchNum, start = match.start(), end = match.end(), 
+        match = match.group()))
+    
