e5f81777afe2026a62d9a402a63c289c7fd7a3e6
Justin Power <33174072+justopower@users.noreply.github.com>
2020-04-26 20:45:35 -0500

    Update test_handshape.py

--- tests/test_handshape.py
+++ tests/test_handshape.py
@@ -4,13 +4,24 @@ Regex for matching handshape symbols.
 """
 import re
 
+# all handshapes in string
 regex = r"[][]*"
 
-test_str = ""
+# only dominant and non-dominant handshapes at start of sign
+dom_nondom = r"(?<!)([][]*)"
 
-matches = re.finditer(regex, test_str)
+# any handshape changes in sign
+handshape_change = r"(?<=)([][]*)"
 
-for matchNum, match in enumerate(matches, start=1):
+# see test_sign.py for target results
+test_example_1 = "   "
+test_example_2 = "    "
+test_example_3 = "                 "
+test_example_4 = "   " 
+
+matches_1 = re.finditer(regex, test_example_1)
+
+for matchNum, match in enumerate(matches_1, start=1):
     
     print ("Match {matchNum} was found at {start}-{end}: {match}".format(
         matchNum = matchNum, start = match.start(), end = match.end(), 
