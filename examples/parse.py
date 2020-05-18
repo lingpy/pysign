f01ecd46c73ba9eb38de0bcf2dc71e0cd198059a
Justin Power <33174072+justopower@users.noreply.github.com>
2020-05-17 19:49:06 -0500

    Update parse.py

--- examples/parse.py
+++ examples/parse.py
@@ -1,10 +1,15 @@
 from pysign.parse import parse_hamnosys, ascify, Sign
 from tabulate import tabulate
 
-strings = ["   ", "   ", 
-        "   ", "    ",
-        "    "
-        ]
+strings = [
+        "   ", # simple, one hand
+        "   ", # two hands, no symmetry
+        "   ", # two hands, simultaneous movement with complex bracketing
+        "    ", # one hand, beginning and ending locations with contact
+        "  ", # contact and repetition 
+        "   ", # one hand, simultaneous movement
+        "   " # detailed initial position, two contacts
+          ]
 
 for string in strings:
     sign = Sign.from_text(string)
